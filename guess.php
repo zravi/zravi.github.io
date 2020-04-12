f2d2dc576494a01878ad522bdd8ba113fae63cf2
ravi <zravi@outlook.com>
2020-04-12 10:36:32 +0530

    Update guess.php

--- guess.php
+++ guess.php
@@ -1,36 +1,40 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ravi Yadav</title>
+  </head>
 <body>
-<title> Ravi Yadav </title>
-<h1> Welcome to my guessing game</h1>
-
-<?php
-$correctnumber=70; 
-if($_GET['guess'])
-{
-  if (is_numeric($_GET['guess'])===FALSE)
- {
-echo "Your guess is not a number";
-}
-else if ($_GET['guess']<$correctnumber)
- {
-echo "Your guess is too low";
-}
-else if ($_GET['guess']>$correctnumber)
- {
-echo "Your guess is too high";
-}
-else if ($_GET['guess']==$correctnumber)
- {
-echo "Congratulations - You are right";
-}
-}
-else
-{
-echo "Missing guess parameter <br> Your guess is too short";
-}
-
-
-
-?>
+  <h1>Welcome to my guessing game</h1>
+  <p>
+    <?php
+      if ( !isset($_GET['guess']) ) 
+      { 
+        echo("Missing guess parameter");
+      } 
+      else if ( strlen($_GET['guess']) < 1 ) 
+      {
+        echo("Your guess is too short");
+      } 
+      else if ( !is_numeric($_GET['guess']) ) 
+      {
+        echo("Your guess is not a number");
+      } 
+      else if ( $_GET['guess'] < 70 ) 
+      {
+        echo("Your guess is too low");
+      } 
+      else if ( $_GET['guess'] > 70 ) 
+      {
+        echo("Your guess is too high");
+      } 
+      else 
+      {
+        echo("Congratulations - You are right");
+      }
+    ?>
+  </p>
 </body>
 </html>
+  
